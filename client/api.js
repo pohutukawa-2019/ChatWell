435505645059bcf29e270c413c09cb8b55c6478d
Katrina Turner <katrinaturner333@gmail.com>
2019-09-26 15:58:44 +1200

    completed ticket - waiting for review and merge

--- client/api.js
+++ client/api.js
@@ -1,7 +1,7 @@
 import request from 'superagent'
 
-export function getTopicsList () {
-  return request.get('/topics')
+export function getTopicsList (bool) {
+  return request.get('/', topics)
   .then(res => {
     const list = res.body
     return list
