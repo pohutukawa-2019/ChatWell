bb006113210c7602212aacb8ec00af8eca3e711b
Henry-Peti <iamgadgetboy@gmail.com>
2019-09-26 15:50:50 +1200

    added disclaimer

--- client/components/Utilities.jsx
+++ client/components/Utilities.jsx
@@ -42,3 +42,7 @@ export function randomAvatar () {
 export function randomColor () {
   return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16)
 }
+
+export function disclaimer () {
+    return [{'This app is intended for support purposes only. It is not intended for use in the diagnosis of conditions or for providing professional advice…… Please seek the advice of healthcare professionals if you are in need. (Provide helpline numbers and contacts)'}]
+}
