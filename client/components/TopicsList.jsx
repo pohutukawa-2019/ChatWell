6329daa01f0666b61abe39aefb02bb636c270bd0
Iris Riddell <riddell.iris@gmail.com>
2019-09-26 16:06:35 +1200

    Start work on TopicsList component

---
+++ client/components/TopicsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+// import { actionGoesHere } from '../actions/actionFile'
+
+class TopicsList extends React.Component {
+  componentDidMount () {
+    this.props.
+  }
+}
