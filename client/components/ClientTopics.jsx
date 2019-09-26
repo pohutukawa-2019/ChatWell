e85dcd29680c4def4662b76e17ddc8ec630df1f7
Iris Riddell <riddell.iris@gmail.com>
2019-09-26 18:23:11 +1200

    Fix linting errors

--- client/components/ClientTopics.jsx
+++ client/components/ClientTopics.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import TopicsList from './TopicsList'
 
 function ClientTopics (props) {
-   return (
-      <div>
-         <h2>I want to talk about...</h2>
-         {props.topics.map(topic => {
-            return <>
-            <TopicsList
-            key={topic.id}
-            topic={topic}/>
+  return (
+    <div>
+      <h2>I want to talk about...</h2>
+      {props.topics.map(topic => {
+        return <>
+         <TopicsList
+           key={topic.id}
+           topic={topic}/>
             </>
-         })}  
-         <Link 
-         className='pure-button'
-         to={'/register'}>Continue</Link>
-      </div>
-   )
-} 
- export default ClientTopics
- 
+      })}
+      <Link className='pure-button' to={'/register'}>Continue</Link>
+    </div>
+  )
+}
+
+export default ClientTopics
--- client/components/SponsorTopics.jsx
+++ client/components/SponsorTopics.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import TopicsList from './TopicsList'
 
 function SponsorTopics (props) {
-   return (
-      <div>
-         <h2>I want to talk about...</h2>
-         {props.topics.map(topic => {
-            return <>
+  return (
+    <div>
+      <h2>I want to help with...</h2>
+      {props.topics.map(topic => {
+        return <>
             <TopicsList
-            key={topic.id}
-            topic={topic}/>
+              key={topic.id}
+              topic={topic}/>
             </>
-         })}  
-         <Link 
-         className='pure-button'
-         to={'/register'}>Continue</Link>
-      </div>
-   )
-} 
- export default SponsorTopics
+      })}
+      <Link className='pure-button' to={'/register'}>Continue</Link>
+    </div>
+  )
+}
+export default SponsorTopics
