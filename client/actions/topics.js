775fa5566b82e4aca845be73d331404f14b8cb89
Iris Riddell <riddell.iris@gmail.com>
2019-09-26 16:05:54 +1200

    Starting work on topics action creator

---
+++ client/actions/topics.js
@@ -0,0 +1,15 @@
+import fetchTopics from '../api/fetchTopics'
+
+export function getTopicsPending () {
+  return {
+    type: GET_TOPIC_PENDING
+  }
+}
+
+export function getTopicsSuccess
+
+export function getTopics () {
+  return dispatch => {
+    dispatch
+  }
+}
