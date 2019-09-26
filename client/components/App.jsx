cfd0c57df3ebbfe36a02d9119edfb92677192066
m-hodges <50115036+m-hodges@users.noreply.github.com>
2019-09-27 11:14:30 +1200

    Update App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ChatRoom from './ChatRoom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import GetHelpButton from './GetHelpButton'
 import ToHelpButton from './ToHelpButton'
@@ -12,7 +11,6 @@ class App extends React.Component {
           <h1>Chat Well</h1>
           <Route path='/' component={GetHelpButton} />
           <Route path='/' component={ToHelpButton} />
-          <ChatRoom />
         </>
       </Router>
     )
