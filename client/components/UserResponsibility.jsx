260cb9c3c649e271cbbde0963f3fb0f4c18bb45f
Iris Riddell <riddell.iris@gmail.com>
2019-09-27 15:43:23 +1200

    added user responsibility component

---
+++ client/components/UserResponsibility.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserResponsibility = () => (
+  <div>
+      Thank you for offering to provide some support and guidance to a fellow human being! With luck, you'll make someone's day a little easier.
+  </div>
+)
+
+export default UserResponsibility
