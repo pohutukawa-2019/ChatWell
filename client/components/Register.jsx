3a80be95c38d81c133a9071b43ddae79525d7e7b
Marvin Coronel <marboynel@gmail.com>
2019-09-29 15:13:25 +1300

    username generating

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,31 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-// import Utilities from './Utilities'
+import { randomName, randomAvatar } from '../utilities'
+import { GridForm, ColOne, ColTwo, Button } from './Styled'
 
 class Register extends React.Component {
-  render () {
-    return (
+    state = {
+      user: {
+        avatar: '',
+        username: ''
+      }
+    }
+
+    handleChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+
+    onSubmit (e) {
+      e.preventDefault()
+      this.setState({
+        username: randomName(),
+        avatar: randomAvatar()
+      })
+    }
+
+    render () {
+      return (
       <>
-      <h2>Create a username and password</h2>
-       <label>Username</label>
-  {/* add username input field below: */}
-        <input type = "text"
-          id = "client-username"
-          value = "your username" />
-       <label>Password</label>
-        <input type = "password"
-          id = "client-password"
-          value = "your secret password" />
-       <h2>OR</h2>
-       <h3>Auto Generate Username</h3>
- {/* link to Utilities component to autogenerate username: */}
-       {/* <Utilities /> */}
- {/* This takes you to the user responsibility/guidance page: */}
-       <Link className='pure-button' to='/guidance'>Continue</Link>
+      <ColOne>Create a username and password</ColOne>
+      <GridForm>
+        <ColOne>Username</ColOne>
+        {/* add username input field below: */}
+        <ColTwo name = "username"
+          value = 'username'
+          onChange={ (e) => { this.handleChange(e) }} />
+        <ColOne>Password</ColOne>
+        <ColTwo name = 'password'
+          value = 'password'
+          onChange={ (e) => { this.handleChange(e) }} />
+        <Button onClick={ (e) => { this.onSubmit(e) }}>Register</Button>
+        <h2>OR</h2>
+        <h3>Auto Generate Username</h3>
+        <input type="submit" name="guestLogin" value="Guest User "
+          onClick={ (e) => { this.onSubmit(e) }}></input>
+        {/* This takes you to the user responsibility/guidance page: */}
+        <Link className='pure-button' to='/guidance'>Guest</Link>
+      </GridForm>
       </>
-    )
+      )
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    username: () => dispatch({ randomName }),
+    avatar: () => dispatch({ randomAvatar })
   }
 }
 
-export default connect()(Register)
+export default connect(mapDispatchToProps)(Register)
