a435593dd51af1591089a11ff33b3003b259038d
marvin-c <marboynel@gmail.com>
2019-09-29 22:37:57 +1300

    checkbox now available at clienttopics.jsx

--- client/components/SponsorTopics.jsx
+++ client/components/SponsorTopics.jsx
@@ -47,7 +47,7 @@ class SponsorTopics extends React.Component {
           )}
         </ul>
         <Link className='pure-button' to='/register'>Continue</Link>
-        <br/>
+        {' '}
         <Link className='pure-button' to='/'>Back to main</Link>
       </div>
     )
