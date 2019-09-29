33582ff00978a29cadbaac6c283b472e5dfca419
Iris Riddell <riddell.iris@gmail.com>
2019-09-29 14:20:55 +1300

    delete fetchTopics and move function into topics.js file

--- client/api/fetchTopics.js
+++
@@ -1,9 +0,0 @@
-import makeRequest from './requestor'
-
-const topicPath = '/topics'
-
-export default function fetchTopics () {
-  return makeRequest(topicPath)
-    .then(topics => topics)
-    .catch(() => { throw new Error('Error accessing topics api.')})
-}
