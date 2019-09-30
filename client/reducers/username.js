5d8929bd183517d40dd1da326ffe1876bb0474ef
Iris Riddell <riddell.iris@gmail.com>
2019-09-30 15:26:13 +1300

    Put userType reducer into combineReducers

--- client/reducers/username.js
+++ client/reducers/username.js
@@ -1,6 +1,6 @@
 import { GET_USERNAME_SUCCESS, SELECTED_USERNAME } from '../actions/username'
 
-export default function usernameReducer (state = [], action) {
+export default function username (state = [], action) {
   switch (action.type) {
     case GET_USERNAME_SUCCESS:
       return action.foods
