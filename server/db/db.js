6abcd479069f5ce1267db585c4c6c73116893e12
Iris Riddell <riddell.iris@gmail.com>
2019-10-01 10:36:59 +1300

    set up db functions for creating new users

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,6 @@
 const connection = require('./connection')
-// const environment = process.env.NODE_ENV || 'development'
-// const config = require('./knexfile')[environment]
-// const connection = require('knex')(config)
+
+const { generateHash } = require('authenticare/server')
 
 function getTopicList (db = connection) {
   return db('topics')
@@ -14,7 +13,39 @@ function getTopicById (id, db = connection) {
     .first()
 }
 
+function createUser (username, password, db = connection) {
+  return userExists(username, db)
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('User exists'))
+      }
+    })
+    .then(() => generateHash(password))
+    .then(passwordHash => {
+      return db('users').insert({ username, hash: passwordHash })
+    })
+}
+
+function userExists (username, db = connection) {
+  return db('users')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
+
+function getUserByName (username, db = connection) {
+  return db('users')
+    .select()
+    .where('username', username)
+    .first()
+}
+
 module.exports = {
   getTopicList,
-  getTopicById
+  getTopicById,
+  createUser,
+  userExists,
+  getUserByName
 }
