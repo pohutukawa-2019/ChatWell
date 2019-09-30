af0e7e584fb6e867bbf06436a1cafb167af60f73
Marvin Coronel <marboynel@gmail.com>
2019-09-30 15:16:03 +1300

    routes repair

--- client/components/SponsorRegister.jsx
+++ client/components/SponsorRegister.jsx
@@ -24,9 +24,9 @@ class SponsorRegister extends React.Component {
  {/* This takes you to the user responsibility/guidance page: */}
      {' '}
     <Link className='pure-button' to='/'>Back to main</Link>
-       <Link className='pure-button' to='/sponsor/guidance'>Continue</Link>
+       <Link className='pure-button' to='/sponsor/pair'>Continue</Link>
        {' '}
-        <Link className='pure-button' to='/'>Back to main</Link>
+      {/* <Link className='pure-button' to='/guidance'>Accept</Link> */}
       </>
     )
   }
