3df4ef69448673cc157cbd0af440ffdb6c94be69
Iris Riddell <riddell.iris@gmail.com>
2019-10-01 10:29:57 +1300

    set up users migration

---
+++ server/db/migrations/users.js
@@ -0,0 +1,12 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.string('username')
+    table.binary('hash')
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('users')
+}
