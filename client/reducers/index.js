9167e6f1847d71ba37e948c714bedca49d4621d5
Iris Riddell <riddell.iris@gmail.com>
2019-09-30 15:22:32 +1300

    Slight rewrite to Guidance, deleted unnecessary reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 
-import showTopics from './showTopics'
 import topics from './topics'
 import error from './error'
 import usernameReducer from './username'
 
 export default combineReducers({
-  showTopics,
   topics,
   usernameReducer,
   error
