dfe318f91d9567f8aa125de41750a4e0c36813db
Marvin Coronel <marboynel@gmail.com>
2019-10-01 12:05:39 +1300

    checked

--- client/reducers/topics.js
+++ client/reducers/topics.js
@@ -1,6 +1,7 @@
 import { GET_TOPIC_SUCCESS, SELECTED_TOPICS } from '../actions/topics'
 
 export default function topicReducer (state = [], action) {
+
   switch (action.type) {
     case GET_TOPIC_SUCCESS:
       return action.topics
