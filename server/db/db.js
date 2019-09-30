10c577f9d6d6970e7d908e31143616a515e61702
Iris Riddell <riddell.iris@gmail.com>
2019-10-01 10:54:29 +1300

    little bit of cleanup in routes, separated auth code into users.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,5 @@
 const connection = require('./connection')
 
-const { generateHash } = require('authenticare/server')
-
 function getTopicList (db = connection) {
   return db('topics')
     .select()
@@ -13,39 +11,7 @@ function getTopicById (id, db = connection) {
     .first()
 }
 
-function createUser (username, password, db = connection) {
-  return userExists(username, db)
-    .then(exists => {
-      if (exists) {
-        return Promise.reject(new Error('User exists'))
-      }
-    })
-    .then(() => generateHash(password))
-    .then(passwordHash => {
-      return db('users').insert({ username, hash: passwordHash })
-    })
-}
-
-function userExists (username, db = connection) {
-  return db('users')
-    .count('id as n')
-    .where('username', username)
-    .then(count => {
-      return count[0].n > 0
-    })
-}
-
-function getUserByName (username, db = connection) {
-  return db('users')
-    .select()
-    .where('username', username)
-    .first()
-}
-
 module.exports = {
   getTopicList,
-  getTopicById,
-  createUser,
-  userExists,
-  getUserByName
+  getTopicById
 }
