08fb7d6f8f1c77af4f367871e6acf8362eb6e55a
Iris Riddell <riddell.iris@gmail.com>
2019-10-01 14:19:18 +1300

    Add Nav component for testing auth log in and out

---
+++ client/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+// import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+import { logOff } from 'authenticare/client'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+
+export default function Nav () {
+  return (
+    <React.Fragment>
+      <Link to='/'>Home</Link>
+      <IfAuthenticated>
+        <Link to='#' data-testid='logoff'
+          onClick={logOff}>Log off</Link>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <Link to='/sponsor/register' data-testid='register'>Register</Link>
+        <Link to='/sponsor/signin' data-testid='signin'>Sign in</Link>
+      </IfNotAuthenticated>
+      <h1>Fruit FTW!</h1>
+    </React.Fragment>
+  )
+}
