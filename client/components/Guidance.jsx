58c587acb3ac4d94cedfe999a257295c7a5470d3
Iris Riddell <riddell.iris@gmail.com>
2019-10-02 12:07:23 +1300

    Added content and resource list in client guidance page

--- client/components/Guidance.jsx
+++ client/components/Guidance.jsx
@@ -6,7 +6,6 @@ import Footer from './Footer'
 import Header from './Header'
 import TitleArea from './elements/TitleArea'
 
-// import TopicListItem from './TopicListItem'
 const theme = {
   primary: '#405d27',
   secondary: '#82b74b',
@@ -19,16 +18,28 @@ function Guidance (props) {
     <div className="hero-client flex-center">
       <ThemeProvider theme={theme}>
         <Header />
-        <TitleArea style={{ fontColor: '#6262B2', textAlign: 'center', fontSize: '28px', fontWeight: 'bold' }}>User Responsibility</TitleArea>
-        <br />
-        <p className="client-font" style={{ textAlign: 'center' }}>
-        This app is intended for support purposes only. It is not intended for use in the diagnosis of conditions or for providing professional advice - remember you are speaking to another human who has been through a lot, so be kind. If you are at risk in any way, please seek the advice of healthcare professionals. (Provide helpline numbers and contacts)
+        <TitleArea style={{ fontColor: '#6262B2', textAlign: 'center', fontSize: '28px', fontWeight: 'bold' }}>Please keep in mind...</TitleArea>
+        <br/>
+        <p className="client-font" style={{ paddingLeft: "200px", paddingRight: "200px", fontSize: "18px", textAlign: 'center' }}>
+        This app is for support purposes only. Remember you are speaking to another human who has been through a lot, so be kind. <br/><br/> 
+        This is not a professional counselling service, simply a way to connect with others who may share your experiences in a safe, completely anonymous environment. <br/><br/>
+
+        If you are at risk in any way, please seek the advice of healthcare professionals.
         </p>
-        {' '}
+        <br/>
+        <h5 className="client-font" style={{ textAlign: 'center', fontSize: '24px' }}>Resources</h5>
+        <ul className="client-font" style={{ textAlign: 'center', fontSize: '14px' }}>
+          <li><a href="https://www.mentalhealth.org.nz/get-help/in-crisis/">Mental Health Foundation of New Zealand</a></li>
+          <li><a href="https://www.lifeline.org.nz/">Lifeline Aotearoa</a></li>
+          <li><a href="https://www.youthline.co.nz/">Youthline</a></li>
+          <li><a href="http://www.samaritans.org.nz/">Samaritans</a></li>
+          <li><a href="https://www.talkingworks.co.nz/dir.html">Talking Works Counselling Service</a></li>
+          <li><a href="https://onlinetherapy.co.nz/">Online Therapy</a></li>
+        </ul>
+        <br />
         <Link to='/pair' style={{ textDecoration: 'none' }}><Button color="primary" style={{ fontFamily: 'Lato', fontWeight: 'bold' }}>CONTINUE</Button></Link>
-        <br />
-        <Link to='/' style={{ textDecoration: 'none' }}><Button color="secondary" style={{ fontFamily: 'Lato', fontWeight: 'bold' }}>BACK TO MAIN</Button></Link>
-        <br />
+        <br/>
+        <br/>
         <Footer />
       </ThemeProvider>
     </div>
