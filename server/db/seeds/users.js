a1570e9fd24098505613cd15afab0680831ddfeb
Iris Riddell <riddell.iris@gmail.com>
2019-10-01 14:24:10 +1300

    Edit user seeds to include generateHash

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -1,17 +1,23 @@
 const { generateHash } = require('authenticare/server')
 
-exports.seed = knex = {
-  // Deletes ALL existing entries
+exports.seed = knex => {
   return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {id: 1, username: 'Iris', hash: irisPass },
-        {id: 2, username: 'Katrina', hash: katrinaPass },
-        {id: 3, username: 'Michael', hash: michaelPass },
-        {id: 4, username: 'Raemon', hash: raemonPass },
-        {id: 5, username: 'Marvin', hash: marvinPass },
-        {id: 6, username: 'Henry', hash: henryPass }
+    .then(() => Promise.all([
+      generateHash('Iris'),
+      generateHash('Katrina'),
+      generateHash('Michael'),
+      generateHash('Raemon'),
+      generateHash('Marvin'),
+      generateHash('Henry')
+    ]))
+    .then(([irisHash, katrinaHash, michaelHash, raemonHash, marvinHash, henryHash]) =>
+      knex('users').insert([
+        { id: 1, username: 'Iris', hash: 'irisHash' },
+        { id: 2, username: 'Katrina', hash: 'katrinaHash' },
+        { id: 3, username: 'Michael', hash: 'michaelHash' },
+        { id: 4, username: 'Raemon', hash: 'raemonHash' },
+        { id: 5, username: 'Marvin', hash: 'marvinHash' },
+        { id: 6, username: 'Henry', hash: 'henryHash' }
       ])
-    })
+    )
 }
